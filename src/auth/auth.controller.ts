db8fe888f2b00959facbbdda73ca4573cf635df4
jgvega10 <jg.vega10@uniandes.edu.co>
2026-05-12 07:58:18 -0500

    P2 Final

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,11 +2,11 @@ import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { RegisterDto } from './dto/register.dto';
 
-@Controller('auth')                  // ⬅️ 'auth' (sin slash)
+@Controller('auth')               
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
-  @Post('register')                  // ⬅️ 'register' (sin slash)
+  @Post('register')                 
   register(@Body() dto: RegisterDto) {
     return this.authService.register(dto);
   }
